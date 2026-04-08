0472066ed77032f3b06628b68082e7c8cf2a7cf8
Damian Valenzuela Negrete <dvnegrete@gmail.com>
2026-04-08 15:33:46 -0600

    fix(environment): Actualizar credenciales de Entra para desarrollo

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,6 +2,6 @@ export const environment = {
   production:     false,
   apiUrl:         'http://localhost:8080',
   authServiceUrl: 'http://localhost:3000',
-  entraClientId:  '<ENTRA_CLIENT_ID>',
-  entraTenantId:  '<ENTRA_TENANT_ID>',
+  entraClientId:  '8d64fe4d-8f99-4adb-a750-865e6a2bcc15',
+  entraTenantId:  'e031f1fa-232a-466c-ba77-923a786de156',
 };
